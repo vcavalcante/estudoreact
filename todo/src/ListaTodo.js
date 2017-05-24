f7ef9a58f57b00a0f60e67d01ea67f73e630cff5
Victor Cavalcante <victor@cavalcante.net>
2017-05-24 03:27:08 -0300

    criado component itemTodo

--- todo/src/ListaTodo.js
+++ todo/src/ListaTodo.js
@@ -1,5 +1,27 @@
 import React,{ Component} from 'react';
 
+class ItemTodo extends Component{
+    constructor(props){
+        super(props);
+        this.state= {todo:props.todo}
+    }
+ 
+    toggleStatus(evento){
+        var todo = this.state.todo ;
+        todo.status = this.state.todo.status === 'incomplete'?'complete':'incomplete';
+        this.setState({todo:todo});
+
+    }
+    render(){     
+        return (
+            <label className={this.state.todo.status}> 
+                <input type='checkbox' defaultChecked = {this.state.todo.status === 'complete'} onChange={() => this.toggleStatus()} /> {this.state.todo.texto} 
+            </label>
+        )
+    }
+     
+}
+
 export default class ListaTodo extends Component{
     constructor(){
         super();
@@ -13,13 +35,10 @@ export default class ListaTodo extends Component{
                 this.props.listaTodo.map(function(todo){
                     return(
                         <li key={todo.id}>
-                            <label className={todo.status}> 
-                                <input type='checkbox' checked = {todo.status === 'complete'} /> {todo.texto} 
-                            </label>
+                            <ItemTodo todo={todo} />
                         </li>);
                 })
             }
-            
             </ul>
         )
     }
